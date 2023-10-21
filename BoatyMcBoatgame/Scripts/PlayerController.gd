629b82b1131f3f2eb29bc777773de59f0a762a7f
Eric <eric.schubert.3d@gmail.com>
2023-10-21 11:28:12 +0200

    Setup 3d player controller, added inputs, imported mesh from kenney

--- BoatyMcBoatgame/Scripts/PlayerController.gd
+++ BoatyMcBoatgame/Scripts/PlayerController.gd
@@ -1,4 +1,7 @@
-extends RigidBody2D
+extends RigidBody3D
+
+var accInput = 0 #input value for fwd and bwd acceleration
+var steerInput = 0 #input value for left and right
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	InputConctroller()
+	print("acc: " + str(accInput))
+	print("steer: " + str(steerInput))
+	
+func InputConctroller():
+	accInput = int(Input.is_action_pressed("MovementFwd")) - int(Input.is_action_pressed("MovementBwd"))
+	steerInput = int(Input.is_action_pressed("MovementLeft")) - int(Input.is_action_pressed("MovementRight"))
