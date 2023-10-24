3eb9231c9748e45e2001e69add219f921ef31fc4
Eric <eric.schubert.3d@gmail.com>
2023-10-24 20:28:50 +0200

    Updated player controller, added first basic level

--- BoatyMcBoatgame/Scripts/PlayerController.gd
+++ BoatyMcBoatgame/Scripts/PlayerController.gd
@@ -1,7 +1,9 @@
 extends RigidBody3D
 
 var input = Vector2.ZERO
-
+@export var maxSpeed = 10.0
+@export var accFac = 50.0
+@export var turnFac = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,19 +13,41 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	InputConctroller()
-	print("acc: " + str(input.y))
-	print("steer: " + str(input.x))
 	
 func _physics_process(delta):
-	ApplyRotation(delta)
+	ApplySteering()
+	KillOrthogonalVelocity()
 	ApplyEngineForce(delta)
 	
 func InputConctroller():
 	input.x =  int(Input.is_action_pressed("MovementLeft")) - int(Input.is_action_pressed("MovementRight"))
 	input.y = int(Input.is_action_pressed("MovementFwd")) - int(Input.is_action_pressed("MovementBwd"))
 
-func ApplyRotation(delta):
-	apply_torque(Vector3(0,1,0) * input.x)
+func ApplySteering():
+	var minSpeedTurn = linear_velocity.length() / 100
+	minSpeedTurn = clamp(minSpeedTurn, 0.0, 1.0)	
+	if input.y < 0.0:
+		input.x *= -1 
+	var rotAngle = input.x * turnFac * minSpeedTurn 
+	rotate_y(rotAngle)
+	
+func KillOrthogonalVelocity():
+	#var fwdVel = -transform.y * linear_velocity.dot(-transform.y)
+	#var rightVel = transform.x * linear_velocity.dot(transform.x)	
+	#linear_velocity = fwdVel + rightVel * driftFac
+	return
 	
 func ApplyEngineForce(delta):
-	apply_force(Vector3(0,0,1) * input.y)
+	var velVsUp = transform.basis.z.dot(linear_velocity)
+	var overspeed = false
+	
+	if velVsUp > maxSpeed && input.y > 0:
+		overspeed = true
+		
+	if velVsUp < -maxSpeed * 0.5 && input.y > 0:
+		overspeed = true
+	
+	if (!overspeed):
+		var engineForce = transform.basis.z  * input.y * accFac
+		#applied_force = engineForce
+		apply_force(engineForce)
